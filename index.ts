6e1e910c14b5c8163af5032888ed68bc6a5fef0d
0x0a0d <vandug23@gmail.com>
2021-12-13 16:19:49 +0700

    Initial commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+import { latestChromeVersion } from 'latest-chrome-version'
+
+export type win_or_mac = 'win' | 'mac'
+
+export async function latestChromeUserAgent(platform: win_or_mac): Promise<string> {
+  const version = await latestChromeVersion(platform)
+  if (platform === 'mac') {
+    return `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${version} Safari/537.36`
+  } else {
+    return `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${version} Safari/537.36`
+  }
+}
