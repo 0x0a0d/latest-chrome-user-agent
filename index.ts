387b79ed005684aea46d807c4f6c2ee46135a96a
0x0a0d <vandug23@gmail.com>
2021-12-22 16:39:51 +0700

    optional platform param

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { latestChromeVersion } from 'latest-chrome-version'
 
 export type win_or_mac = 'win' | 'mac'
 
-export async function latestChromeUserAgent(platform: win_or_mac): Promise<string> {
+export async function latestChromeUserAgent(platform?: win_or_mac): Promise<string> {
   const version = await latestChromeVersion(platform)
   if (platform === 'mac') {
     return `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${version} Safari/537.36`
